1eda7f04ac423b3176e2f3e855d0caa79f74cf45
tola <79221297+TolaMironcenko@users.noreply.github.com>
2022-03-19 02:38:11 +0700

    commit

--- js/app.js
+++ js/app.js
@@ -1,15 +1,12 @@
 const navbar = document.querySelector('.navbar')
 
-const removeActiveClasses = () => {
-	const navlinks = document.querySelector('.navbar').querySelectorAll('.navlink')
-	navlinks.forEach((navlink) => {
-		navlink.classList.remove('active')
-	})
-}
-
 navbar.addEventListener('click', (event) => {
 	if (event.target.classList.contains('navlink')) {
-		removeActiveClasses()
+		// removeActiveClasses()
+		const activeLink = document.querySelector('.active')
+		if (activeLink) {
+			activeLink.classList.remove('active')
+		}
 		event.target.classList.add('active')
 	}
 })
